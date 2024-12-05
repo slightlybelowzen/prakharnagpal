bf6b5d434f1ba517b1b5c627ab853e06002418b5
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2024-12-05 04:52:16 +0000

    adding tatem scroll component

--- src/components/experiments/tatemScroll.jsx
+++ src/components/experiments/tatemScroll.jsx
@@ -1,5 +1,39 @@
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 export default function TatemScroll() {
-  return <motion.div>Hello</motion.div>;
+  const data = [
+    {
+      label: "Inbox",
+      count: 30,
+    },
+    {
+      label: "Meetings",
+      count: 12,
+    },
+    {
+      label: "Subscriptions",
+      count: 8,
+    },
+    {
+      label: "Newsletters",
+      count: 5,
+    },
+    {
+      label: "Shopping",
+      count: 4,
+    },
+  ];
+  return (
+    <motion.div className="group bg-[#00000008] min-h-72 rounded-lg shadow-code flex flex-col gap-2 justify-center items-center px-14">
+      <div className="flex gap-5">
+        {data.map((item) => (
+          <div className="flex items-center gap-1.5 justify-center text-body-muted">
+            <div>{item.label}</div>
+            <div className="text-sm text-graphic-outline">{item.count}</div>
+          </div>
+        ))}
+      </div>
+      <div className="relative h-0.5 w-full items-center bg-beige-dark rounded-full"></div>
+    </motion.div>
+  );
 }
