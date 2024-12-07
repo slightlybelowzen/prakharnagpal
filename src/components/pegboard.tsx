95d934c45ad4b05d15656a1962d3fc1ebffeeaaa
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2024-12-07 01:43:23 +0000

    grid

--- src/components/pegboard.tsx
+++ src/components/pegboard.tsx
@@ -1,12 +1,19 @@
 export default function PegBoard() {
   return (
-    <div className="relative w-full h-full min-h-[36vh] border border-beige-dark/50 px-6 py-4 rounded-sm">
-      <div className="absolute inset-0 h-full w-full bg-beige-alt/25 bg-[linear-gradient(to_right,var(--color-beige)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-beige)_1px,transparent_1px)] bg-[size:24px_24px]">
-        <div className="relative h-full w-full">
-          <div className="absolute top-5 bg-beige-dark/40 w-20 h-20"></div>
-          <div className="absolute top-20 left-18 bg-beige-dark/40 w-20 h-20"></div>
-          <div className="absolute top-40 left-40 bg-beige-dark/40 w-20 h-20"></div>
-        </div>
+    <div className="overflow-hidden relative w-full h-full min-h-[72vh] bg-beige-alt/80 border border-beige-dark/50 rounded-sm">
+      <div className="absolute top-0 left-0 w-full h-20 grid-header grid-size-24" />
+      <div className="overflow-hidden absolute h-full w-full grid grid-size-24" />
+      <div className="relative h-full w-full">
+        {/* {homePageData.map((note) => (
+          <Note
+            key={note.title}
+            pos={{ left: getRandomInt(0, 100), top: getRandomInt(0, 100) }}
+            title={note.title}
+            description={note.description}
+            external={note.external}
+            link={note.link}
+          />
+        ))} */}
       </div>
     </div>
   );
