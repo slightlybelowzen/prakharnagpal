cd17d19638bab1e13d1c9bd64eb0a28308170b91
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2024-12-10 04:31:39 +0000

    going back to this

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import vercelLight from "./src/utils/themes/vercel-light.json";
 
 // https://astro.build/config
@@ -11,7 +10,6 @@ export default defineConfig({
   prefetch: true,
   integrations: [react(), mdx()],
   markdown: {
-    rehypePlugins: [rehypeHeadingIds],
     shikiConfig: {
       theme: vercelLight,
     },
